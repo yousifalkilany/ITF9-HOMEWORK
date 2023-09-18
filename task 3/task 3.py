c7e6e2e1caa2a566c9bf7dcee284247e56f363e4
yousifalkilany <yousif.alkilany@gmail.com>
2023-09-18 04:50:43 -0700

    task 3

--- task 3/task 3.py
+++ task 3/task 3.py
@@ -1,6 +1,22 @@
-x = 20
-y = 50
-print(x + y)
-print(x - y)
-print(x - y)
-print(y * x)
+print("ADD")
+def add(x=0,y=0):
+    total = x + y
+    print(total)
+num1 = int(input("Enter Number 1 :"))
+num2 = int(input("Enter Number 2 :"))
+add(num1,num2)
+print("SUB")
+def sub(x=0,y=0):
+    total = x - y
+    print(total)
+num1 = int(input("Enter Number 1 :"))
+num2 = int(input("Enter Number 2 :"))
+sub(num1,num2)
+print("DIV")
+def div(x=0,y=0):
+    total = x / y
+    print(total)
+num1 = int(input("Enter Number 1 :"))
+num2 = int(input("Enter Number 2 :"))
+div(num1,num2)
+print("MUL")
