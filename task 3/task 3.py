1de206672227449c89975891a62d8d42fbb846e9
yousifalkilany <yousif.alkilany@gmail.com>
2023-09-23 02:10:56 -0700

    TASK4

--- task 3/task 3.py
+++ task 3/task 3.py
@@ -1,22 +1,49 @@
-print("ADD")
-def add(x=0,y=0):
+def add(x, y):
     total = x + y
-    print(total)
-num1 = int(input("Enter Number 1 :"))
-num2 = int(input("Enter Number 2 :"))
-add(num1,num2)
-print("SUB")
-def sub(x=0,y=0):
+    return total
+
+
+def sub(x, y):
     total = x - y
-    print(total)
-num1 = int(input("Enter Number 1 :"))
-num2 = int(input("Enter Number 2 :"))
-sub(num1,num2)
-print("DIV")
-def div(x=0,y=0):
+    return total
+
+
+def mul(x, y):
+    total = x * y
+    return total
+
+
+def div(x, y):
     total = x / y
-    print(total)
-num1 = int(input("Enter Number 1 :"))
-num2 = int(input("Enter Number 2 :"))
-div(num1,num2)
-print("MUL")
+    return total
+
+
+x = int(input("Enter Number 1 :"))
+y = int(input("Enter Nmuber 2 :"))
+print("Total after ADD :", add(x, y))
+print("Total after SUB :", sub(x, y))
+print("Total after MUL :", mul(x, y))
+print("Total after DIV :", div(x, y))
+print("----------------- part 2 -----------------")
+print("""If You Want to Calculate rectangle area type >> 1 <
+If You Want to Calculate circle area type >> 2 <
+If You Want to Calculate triangle area type >> 3 <
+If not type >> 0 <""")
+N = int(input())
+if N == 1:
+    L = int(input("Enter the length :"))
+    W = int(input("Enter the width :"))
+    sum = L * W
+    print("The area is : ", sum)
+elif N == 2:
+    R = int(input("Enter the Radius :"))
+    sum = 3.14 * R * R
+    print("The area is : ", sum)2020
+elif N == 3:
+    B = int(input("Enter the Base :"))
+    H = int(input("Enter the Height :"))
+    sum = B * H / 2
+    print("The area is : ", sum)
+
+else:
+    print("^_^ THANKS YOU ^_^")
